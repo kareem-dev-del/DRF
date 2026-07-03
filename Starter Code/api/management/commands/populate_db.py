ecd4bcdfb70ad3592968b1e4c65b53e43b92519e
kareem-dev-del <krym33632@gmail.com>
2026-07-03 20:19:53 +0300

    add

--- Starter Code/api/management/commands/populate_db.py
+++ Starter Code/api/management/commands/populate_db.py
@@ -2,7 +2,6 @@ import random
 from decimal import Decimal
 
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 from django.utils import lorem_ipsum
 from api.models import User, Product, Order, OrderItem
 
