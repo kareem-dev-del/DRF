6ff4fe80491da54bfaa991baaecc2ea752da70b3
kareem-dev-del <krym33632@gmail.com>
2026-09-21 21:59:34 +0300

    API Documentation

--- Starter Code/api/urls.py
+++ Starter Code/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
    # path('products/', views.product_list),
     path('products/', views.ProductListAPIView.as_view()),
--- Starter Code/drf_course/settings.py
+++ Starter Code/drf_course/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'silk',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -122,5 +123,14 @@ REST_FRAMEWORK = {
        
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Products API',
+    'DESCRIPTION': 'API for managing products',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
